02346564be53e190f858aee8035787f191dcae24
Vitaliy <akvamiris@yahoo.com>
2016-11-19 01:48:05 +0200

    yii2-shop migrate2

--- frontend/config/main.php
+++ frontend/config/main.php
@@ -13,9 +13,17 @@ return [
     'controllerNamespace' => 'frontend\controllers',
     'defaultRoute' => 'catalog/list',
     'components' => [
+        'request' => [
+            'csrfParam' => '_csrf-frontend',
+        ],
         'user' => [
             'identityClass' => 'common\models\User',
             'enableAutoLogin' => true,
+            'identityCookie' => ['name' => '_identity-frontend', 'httpOnly' => true],
+        ],
+        'session' => [
+            // this is the name of the session cookie used for login on the frontend
+            'name' => 'advanced-frontend',
         ],
         'log' => [
             'traceLevel' => YII_DEBUG ? 3 : 0,
@@ -32,6 +40,22 @@ return [
         'cart' => [
             'class' => 'yz\shoppingcart\ShoppingCart',
         ],
+        /*
+        'urlManager' => [
+            'enablePrettyUrl' => true,
+            'showScriptName' => false,
+            'rules' => [
+            ],
+        ],
+        */
+        'urlManager' => [
+            'enablePrettyUrl' => true,
+            'showScriptName' => false,
+            'rules' => [
+                '' => 'site/index',
+                '<controller:\w+>/<action:\w+>/' => '<controller>/<action>',
+            ],
+        ]
     ],
     'params' => $params,
 ];
