c35853b0aad71df9fa4405944d67115218a3f55f
paparasha <Sashaandbitbucket1>
2017-04-24 23:30:09 +0300

    Test Contact Form part FIVE 2

--- frontend/views/contact/index.php
+++ frontend/views/contact/index.php
@@ -227,7 +227,7 @@ AIzaSyAxsOMhMNNlJe38h-ON-0MkOxBLCT78MRU&callback=initMap"></script>
                     mail($to, $subject, $message, $header);
 
                     // Step 4
-                    header('Location: http://edumy.co.cc/Thankyou.html');
+                    header('Location: http://iq-offshore.com/contact');
 
                     ?>
 
