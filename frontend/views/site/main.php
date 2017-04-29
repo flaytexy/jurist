e8d0ed6a748e9ef5f724da737adc63fdf84112da
paparasha <Sashaandbitbucket1>
2017-04-30 00:10:49 +0300

    New "Buy" and "detail" on main

--- frontend/views/site/main.php
+++ frontend/views/site/main.php
@@ -473,11 +473,11 @@ MainAsset::register($this);
                                                 <h4><a href="/offers/<?= $offer->model->slug ?>"
                                                        title="<?= $offer->model->title ?>"><?= $offer->model->title ?></a></h4>
                                                 <ul class="location-book">
-                                                    <li><i class="fa fa-map-marker"></i>
-                                                        <span>Подробнее</span></li>
-                                                    <li class="book-btn"><i class="fa fa-shopping-basket"></i><a
-                                                            href="/offers/<?= $offer->model->slug ?>"
-                                                            title="">Подробнее</a></li>
+                                                    <li><i class="fa fa-info"></i>
+                                                        <span class="blacklink"><a
+                                                                href="/offers/<?= $offer->model->slug ?>"
+                                                                title="">Подробнее</a></span></li>
+                                                    <li class="book-btn"><i class="fa fa-shopping-basket"></i> <a href="javascript:void( window.open( 'https://form.jotformeu.com/71136944138357', 'blank', 'scrollbars=yes, toolbar=no, width=700, height=700, align=center' ) )">Заказать</a></li>
                                                 </ul>
                                             </div>
                                         </div>
