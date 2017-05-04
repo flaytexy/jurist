fd747631dba66dab41bb2a05dc6a2491cbe336a3
paparasha <Sashaandbitbucket1>
2017-05-04 19:33:30 +0300

    deleted div at main

--- frontend/views/site/main.php
+++ frontend/views/site/main.php
@@ -385,7 +385,6 @@ MainAsset::register($this);
 </style>
 
 <section>
-    <div class="centered">
     <div class="block gray no-padding">
         <div class="container">
             <div class="row">
@@ -447,11 +446,10 @@ MainAsset::register($this);
             </div>
         </div>
     </div>
-        </div>
 </section>
 
 <section>
-
+    <div class="block ext-toppadd-one">
         <div class="fixed-bg2" style="background-image: url(/uploads/theme_villa/parallax2.jpg);"></div>
         <div class="container">
             <div class="row">
