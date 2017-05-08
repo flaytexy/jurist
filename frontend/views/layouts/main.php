3c423055cb53e5d93f65a5c8268fef1e59e45428
paparasha <Sashaandbitbucket1>
2017-05-08 13:21:59 +0300

    limit popularity

--- frontend/views/layouts/main.php
+++ frontend/views/layouts/main.php
@@ -9,7 +9,7 @@ use yii\helpers\Html;
 use yii\helpers\Url;
 use yii\widgets\ActiveForm;
 
-$popularly = \frontend\models\Popularly::find()->limit(12)->all();
+$popularly = \frontend\models\Popularly::find()->limit(10)->all();
 
 $phoneStr = "+7 925 470 50 02";
 
