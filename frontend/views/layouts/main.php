4823c70e10d85a433d690053ed95f74b706ddaa5
paparasha <Sashaandbitbucket1>
2017-05-26 13:13:36 +0300

    Scrolling animation6back

--- frontend/views/layouts/main.php
+++ frontend/views/layouts/main.php
@@ -19,7 +19,7 @@ $phoneStr = "+7 925 470 50 02";
 
 <script src="//load.sumome.com/" data-sumo-site-id="3e9ad4ed5127b8e285ee649aa55e8340bf5eb21a370f52999a717953ee42fd89" async="async"></script>
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-<script src="https://code.jquery.com/jquery-3.2.1.js"></script>
+
 
 
 <link href="https://fonts.googleapis.com/css?family=Cabin" rel="stylesheet">
