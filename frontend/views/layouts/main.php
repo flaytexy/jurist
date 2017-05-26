413cb75508df61d3994174a7b394e7f669de972b
paparasha <Sashaandbitbucket1>
2017-05-26 07:54:08 +0300

    Scrolling animation5

--- frontend/views/layouts/main.php
+++ frontend/views/layouts/main.php
@@ -16,6 +16,7 @@ $phoneStr = "+7 925 470 50 02";
 
 ?>
 <?php $this->beginContent('@app/views/layouts/base.php'); ?>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
 <script src="//load.sumome.com/" data-sumo-site-id="3e9ad4ed5127b8e285ee649aa55e8340bf5eb21a370f52999a717953ee42fd89" async="async"></script>
 <link href="https://fonts.googleapis.com/css?family=Cabin" rel="stylesheet">
 <div class="js"><div id="preloader"><hr class="hr-text" data-content="IQ Decision">
