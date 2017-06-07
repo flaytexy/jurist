ee8e68cf2668b0eca155a764841d03537c025f48
paparasha <Sashaandbitbucket1>
2017-06-07 21:13:26 +0300

    delete preload function

--- frontend/views/layouts/main.php
+++ frontend/views/layouts/main.php
@@ -18,13 +18,13 @@ $phoneStr = "+7 925 470 50 02";
 <?php $this->beginContent('@app/views/layouts/base.php'); ?>
 <script src="//load.sumome.com/" data-sumo-site-id="3e9ad4ed5127b8e285ee649aa55e8340bf5eb21a370f52999a717953ee42fd89" async="async"></script>
 <link href="https://fonts.googleapis.com/css?family=Cabin" rel="stylesheet">
-<div class="js"><div id="preloader"><hr class="hr-text" data-content="IQ Decision">
+<!-- <div class="js"><div id="preloader"><hr class="hr-text" data-content="IQ Decision">
        <div class="preloader">
             <div class="circ1"></div>
             <div class="circ2"></div>
             <div class="circ3"></div>
             <div class="circ4"></div>
-        </div></div></div>
+        </div></div></div> -->
 <style>
 
 
@@ -1018,13 +1018,13 @@ AIzaSyAxsOMhMNNlJe38h-ON-0MkOxBLCT78MRU&callback=initMap"></script>
 
 
 // site preloader -- also uncomment the div in the header and the css style for #preloader
-        $(window).load(function(){
+     /*   $(window).load(function(){
 
             $('#rhlpscrtg').fadeOut('slow',function(){$(this).show();});
             $("#status").fadeOut();
             $('#preloader').delay(1000).fadeOut('slow',function(){$(this).remove();});
 
-        });
+        }); */
         setTimeout(function() {
             var script = document.createElement('script');
             script.src = '//widget.privy.com/assets/widget.js';
