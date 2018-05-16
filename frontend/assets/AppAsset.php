8c40e2449bf02704104e6bcf0bb806d723f9c356
ETREX <akvamiris@yahoo.com>
2018-05-16 20:38:50 +0300

    styles.css 3 h1 h2

--- frontend/assets/AppAsset.php
+++ frontend/assets/AppAsset.php
@@ -48,7 +48,7 @@ class AppAsset extends AssetBundle
         'css/animate.3.2.3.css', //*last
 //        //'css/prefix.min.css?v=2017-02-25-2',
         'css/prefix2.css?v=2018-01-18-v01', //*last
-        'css/styles.css?v=2018-05-16-v03', //*last
+        'css/styles.css?v=2018-05-16-v04', //*last
 //        'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css',
 
 
@@ -105,7 +105,7 @@ class AppAsset extends AssetBundle
     {
         if (!YII_DEBUG) {
             $this->css = [
-                'css/style_all.min.css?v=2018-05-16-v03'
+                'css/style_all.min.css?v=2018-05-16-v04'
             ];
         }
 
