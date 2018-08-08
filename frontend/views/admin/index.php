e8c6d6b514b7aa9274290bcd3ffc4c1437594649
ETREX <akvamiris@yahoo.com>
2018-08-08 12:10:38 +0300

    admin_ka remove

--- frontend/views/admin/index.php
+++
@@ -1,2 +0,0 @@
-<?php
-echo 'qesads';exit;
--- frontend/views/admin_ka/category/edit.php
+++
@@ -1,7 +0,0 @@
-<?php
-$this->title = Yii::t('easyii', 'Edit category');
-?>
-<?= $this->render('_menu') ?>
-
-<?php if(!empty($this->params['submenu'])) echo $this->render('_submenu', ['model' => $model], $this->context); ?>
-<?= $this->render('_form', ['model' => $model]) ?>
